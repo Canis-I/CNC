fe2d5e4259f747f2c82fe2a9b002b792819f050f
rc-regalado <ruben.alekey@gmail.com>
2021-05-28 18:53:50 -0600

    Asignacion de los pines para mosfet de taladros

--- sources/grbl/cpu_map.h
+++ sources/grbl/cpu_map.h
@@ -19,20 +19,21 @@
   along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* The cpu_map.h files serve as a central pin mapping selection file for different 
-   processor types or alternative pin layouts. This version of Grbl supports only the 
-   Arduino Mega2560. */
+/*
+ * El archivo cpu_map.h funciona como archivo central para el mapa de pines.
+ * Esta version solo funciona en el Arduino Mega2560
+ */
 
 #ifndef cpu_map_h
 #define cpu_map_h
 // (Arduino Mega 2560) with Ramps 1.4 Board
 #include "nuts_bolts.h"
 
-// Serial port interrupt vectors
+// Vectores de interrucion de los puertos seriales
 #define SERIAL_RX USART0_RX_vect
 #define SERIAL_UDRE USART0_UDRE_vect
 
-// Define ports and pins
+// Define puertos y pines
 #define DDR(port) DDR##port
 #define _DDR(port) DDR(port)
 #define PORT(port) PORT##port
@@ -40,8 +41,7 @@
 #define PIN(pin) PIN##pin
 #define _PIN(pin) PIN(pin)
 
-// Define step pulse output pins.
-
+// Define pines de salida para pasos.
 #define STEP_PORT_0 F
 #define STEP_PORT_1 F
 #define STEP_PORT_2 L
@@ -55,7 +55,7 @@
 #define STEP_PORT(i) _STEP_PORT(i)
 #define STEP_PIN(i) _PIN(STEP_PORT_##i)
 
-// Define step direction output pins.
+// Define pines de salida para direccion de pasos
 #define DIRECTION_PORT_0 F
 #define DIRECTION_PORT_1 F
 #define DIRECTION_PORT_2 L
@@ -69,7 +69,7 @@
 #define DIRECTION_PORT(i) _DIRECTION_PORT(i)
 #define DIRECTION_PIN(i) _PIN(DIRECTION_PORT_##i)
 
-// Define stepper driver enable/disable output pin.
+// Define el pin de habilitar/deshabilitar
 #define STEPPER_DISABLE_PORT_0 D
 #define STEPPER_DISABLE_PORT_1 F
 #define STEPPER_DISABLE_PORT_2 K
@@ -81,7 +81,8 @@
 #define STEPPER_DISABLE_PORT(i) _PORT(STEPPER_DISABLE_PORT_##i)
 #define STEPPER_DISABLE_PIN(i) _PIN(STEPPER_DISABLE_PORT_##i)
 
-// Define homing/hard limit switch input pins and limit interrupt vectors.
+// Define los pines de finales de carrera
+// y vectores sus de interrupciones
 #define MIN_LIMIT_PORT_0 E
 #define MIN_LIMIT_PORT_1 J
 #define MIN_LIMIT_PORT_2 D
@@ -112,7 +113,7 @@
 //  #define LIMIT_MASK ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
 #define DISABLE_HW_LIMITS
 
-// Define spindle enable and spindle direction output pins.
+// Define los pines de los servomotores.
 #define SPINDLE_ENABLE_DDR      DDRG
 #define SPINDLE_ENABLE_PORT     PORTG
 #define SPINDLE_ENABLE_BIT      5 // MEGA2560 Digital Pin 4 - Ramps 1.4 Servo 4 Signal pin
@@ -120,16 +121,17 @@
 #define SPINDLE_DIRECTION_PORT  PORTE
 #define SPINDLE_DIRECTION_BIT   3 // MEGA2560 Digital Pin 5 - Ramps 1.4 Servo 3 Signal pin
 
-// Define flood and mist coolant enable output pins.
-#define COOLANT_FLOOD_DDR   DDRB
-#define COOLANT_FLOOD_PORT  PORTB
-#define COOLANT_FLOOD_BIT   4 // MEGA2560 Digital Pin 10 - Ramps 1.4 12v output
-#define COOLANT_MIST_DDR    DDRH
-#define COOLANT_MIST_PORT   PORTH
-#define COOLANT_MIST_BIT    6 // MEGA2560 Digital Pin 9 - Ramps 1.4 12v output
+// Define los pines de salida del taladro.
+#define DRILL_FLOOD_DDR   DDRB
+#define DRILL_FLOOD_PORT  PORTB
+#define DRILL_FLOOD_BIT   4 // MEGA2560 Digital Pin 10 - Ramps 1.4 12v output
+#define DRILL_MIST_DDR    DDRH
+#define DRILL_MIST_PORT   PORTH
+#define DRILL_MIST_BIT    6 // MEGA2560 Digital Pin 9 - Ramps 1.4 12v output
 
-// Define user-control CONTROLs (cycle start, reset, feed hold) input pins.
-// NOTE: All CONTROLs pins must be on the same port and not on a port with other input pins (limits).
+// Define los pines de control de usuario.
+// Nota: Todos los controles deben estar en el mismo puerto y no en un puerto
+// con otros pines de entrada.
 #define CONTROL_DDR       DDRK
 #define CONTROL_PIN       PINK
 #define CONTROL_PORT      PORTK
@@ -142,15 +144,15 @@
 #define CONTROL_PCMSK     PCMSK2 // Pin change interrupt register
 #define CONTROL_MASK      ((1<<CONTROL_RESET_BIT)|(1<<CONTROL_FEED_HOLD_BIT)|(1<<CONTROL_CYCLE_START_BIT)|(1<<CONTROL_SAFETY_DOOR_BIT))
 
-// Define probe switch input pin.
+// Define el pin de entrada del interruptor.
 #define PROBE_DDR       DDRK
 #define PROBE_PIN       PINK
 #define PROBE_PORT      PORTK
 #define PROBE_BIT       7  // MEGA2560 Analog Pin 15
 #define PROBE_MASK      (1<<PROBE_BIT)
 
-// Advanced Configuration Below You should not need to touch these variables
-// Set Timer up to use TIMER4B which is attached to Digital Pin 8 - Ramps 1.4 12v output with heat sink
+// Configuracion avanzada, estas configuraciones no deberian ser tocadas
+// Configurar el temporalizador TIME4B que esta enlazado al pin digital 8 - Ramps 1.4 12v output with heat sink
 #define SPINDLE_PWM_MAX_VALUE     1024.0 // Translates to about 1.9 kHz PWM frequency at 1/8 prescaler
 #ifndef SPINDLE_PWM_MIN_VALUE
 #define SPINDLE_PWM_MIN_VALUE   1   // Must be greater than zero.
@@ -158,7 +160,7 @@
 #define SPINDLE_PWM_OFF_VALUE     0
 #define SPINDLE_PWM_RANGE         (SPINDLE_PWM_MAX_VALUE-SPINDLE_PWM_MIN_VALUE)
 
-//Control Digital Pin 6 which is Servo 2 signal pin on Ramps 1.4 board
+//Control Digital Pin 6 que es el pin de señal del Servo 2 en la Ramps 1.4 board
 #define SPINDLE_TCCRA_REGISTER    TCCR4A
 #define SPINDLE_TCCRB_REGISTER    TCCR4B
 #define SPINDLE_OCR_REGISTER      OCR4C
@@ -170,7 +172,7 @@
 #define SPINDLE_OCRA_REGISTER   OCR4A // 16-bit Fast PWM mode requires top reset value stored here.
 #define SPINDLE_OCRA_TOP_VALUE  0x0400 // PWM counter reset value. Should be the same as PWM_MAX_VALUE in hex.
 
-// Define spindle output pins.
+// Define los pines de salida del husillo
 #define SPINDLE_PWM_DDR   DDRH
 #define SPINDLE_PWM_PORT  PORTH
 #define SPINDLE_PWM_BIT   5 // MEGA2560 Digital Pin 8
