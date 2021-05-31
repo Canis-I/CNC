8eea2712dd439e7d50e9656678d90f4603cc7640
rc-regalado <ruben.alekey@gmail.com>
2021-05-30 23:24:25 -0600

    Adaptado a la RAMPS 1.4. Falta traduccion.

--- sources/grbl/grbl.h
+++ sources/grbl/grbl.h
@@ -2,7 +2,8 @@
   grbl.h - main Grbl include file
   Part of Grbl
 
-  Copyright (c) 2015 Sungeun K. Jeon
+  Copyright (c) 2015-2016 Sungeun K. Jeon
+  Copyright (c) 2021 Canis I
 
   Grbl is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -21,31 +22,32 @@
 #ifndef grbl_h
 #define grbl_h
 
-// Grbl versioning system
-#define GRBL_VERSION "0.9j"
-#define GRBL_VERSION_BUILD "20160726"
+// Versiones
+#define GRBL_VERSION "0.1-alpha"
+#define GRBL_VERSION_BUILD "20210530.Canis"
 
-// Define standard libraries used by Grbl.
+// Define las librerias usadas por  Grbl
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
 #include <math.h>
-#include <inttypes.h>    
+#include <inttypes.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-// Define the Grbl system include files. NOTE: Do not alter organization.
+// Define el sistema de archivos de Grbl
 #include "config.h"
 #include "nuts_bolts.h"
 #include "settings.h"
 #include "system.h"
 #include "defaults.h"
 #include "cpu_map.h"
-#include "coolant_control.h"
+#include "planner.h"
+#include "drill_control.h"
 #include "eeprom.h"
 #include "gcode.h"
 #include "limits.h"
@@ -58,5 +60,39 @@
 #include "serial.h"
 #include "spindle_control.h"
 #include "stepper.h"
+#include "jog.h"
+#include "sleep.h"
+
+// ---------------------------------------------------------------------------------------
+// COMPILE-TIME ERROR CHECKING OF DEFINE VALUES:
+
+#ifndef HOMING_CYCLE_0
+  #error "Required HOMING_CYCLE_0 not defined."
+#endif
+
+#if defined(PARKING_ENABLE)
+  #if defined(HOMING_FORCE_SET_ORIGIN)
+    #error "HOMING_FORCE_SET_ORIGIN is not supported with PARKING_ENABLE at this time."
+  #endif
+#endif
+
+#if defined(SPINDLE_PWM_MIN_VALUE)
+  #if !(SPINDLE_PWM_MIN_VALUE > 0)
+    #error "SPINDLE_PWM_MIN_VALUE must be greater than zero."
+  #endif
+#endif
+
+#if (REPORT_WCO_REFRESH_BUSY_COUNT < REPORT_WCO_REFRESH_IDLE_COUNT)
+  #error "WCO busy refresh is less than idle refresh."
+#endif
+#if (REPORT_OVR_REFRESH_BUSY_COUNT < REPORT_OVR_REFRESH_IDLE_COUNT)
+  #error "Override busy refresh is less than idle refresh."
+#endif
+#if (REPORT_WCO_REFRESH_IDLE_COUNT < 2)
+  #error "WCO refresh must be greater than one."
+#endif
+#if (REPORT_OVR_REFRESH_IDLE_COUNT < 1)
+  #error "Override refresh must be greater than zero."
+#endif
 
 #endif
