e135e4b838e3f0bff6bdc4394890658a1d0127c4
rc-regalado <ruben.alekey@gmail.com>
2021-05-30 23:03:04 -0600

    Adaptado a la RAMPS 1.4. Falta traduccion.

--- sources/grbl/defaults.h
+++ sources/grbl/defaults.h
@@ -2,7 +2,8 @@
   defaults.h - defaults settings configuration file
   Part of Grbl
 
-  Copyright (c) 2012-2015 Sungeun K. Jeon
+  Copyright (c) 2012-2016 Sungeun K. Jeon
+  Copyright (c) 2021 Canis I
 
   Grbl is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -23,69 +24,49 @@
    files listed here are supplied by users, so your results may vary. However, this should
    give you a good starting point as you get to know your machine and tweak the settings for
    your nefarious needs.
-   Ensure one and only one of these DEFAULTS_XXX values is defined in config.h */
+   NOTE: Ensure one and only one of these DEFAULTS_XXX values is defined in config.h */
 
 #ifndef defaults_h
 
-// Only define the DEFAULT_XXX with where to find the corresponding default_XXX.h file.
-// Don't #define defaults_h here, let the selected file do it. Prevents including more than one.
-
-#ifdef DEFAULTS_GENERIC
-  // Grbl generic default settings. Should work across different machines.
-  #include "defaults/defaults_generic.h"
-#endif
-
-#ifdef DEFAULTS_SHERLINE_5400
-  // Description: Sherline 5400 mill with three NEMA 23 Keling  KL23H256-21-8B 185 oz-in stepper motors,
-  // driven by three Pololu A4988 stepper drivers with a 30V, 6A power supply at 1.5A per winding.
-  #include "defaults/defaults_sherline.h"
-#endif
-
-#ifdef DEFAULTS_SHAPEOKO
-  // Description: Shapeoko CNC mill with three NEMA 17 stepper motors, driven by Synthetos
-  // grblShield with a 24V, 4.2A power supply.
-  #include "defaults/defaults_shapeoko.h"
-#endif
-
-#ifdef DEFAULTS_SHAPEOKO_2
-  // Description: Shapeoko CNC mill with three NEMA 17 stepper motors, driven by Synthetos
-  // grblShield at 28V.
-  #include "defaults/defaults_shapeoko2.h"
-#endif
-
-#ifdef DEFAULTS_SHAPEOKO_3
-  // Description: Shapeoko CNC mill with three NEMA 23 stepper motors, driven by CarbideMotion
-  #include "defaults/defaults_shapeoko3.h"
-#endif
-
-#ifdef DEFAULTS_X_CARVE_500MM
-  // Description: X-Carve 3D Carver CNC mill with three 200 step/rev motors driven by Synthetos
-  // grblShield at 24V.
-  #include "defaults/defaults_x_carve_500mm.h"
-#endif
-
-#ifdef DEFAULTS_X_CARVE_1000MM
-  // Description: X-Carve 3D Carver CNC mill with three 200 step/rev motors driven by Synthetos
-  // grblShield at 24V.
-  #include "defaults/defaults_x_carve_1000mm.h"
-#endif
-
-#ifdef DEFAULTS_ZEN_TOOLWORKS_7x7
-  // Description: Zen Toolworks 7x7 mill with three Shinano SST43D2121 65oz-in NEMA 17 stepper motors.
-  // Leadscrew is different from some ZTW kits, where most are 1.25mm/rev rather than 8.0mm/rev here.
-  // Driven by 30V, 6A power supply and TI DRV8811 stepper motor drivers.
-  #include "defaults/defaults_zen_toolworks_7x7.h"
-#endif
-
-#ifdef DEFAULTS_OXCNC
-  // Grbl settings for OpenBuilds OX CNC Machine
-  // http://www.openbuilds.com/builds/openbuilds-ox-cnc-machine.341/
-  #include "defaults/defaults_oxcnc.h"
-#endif
-
-#ifdef DEFAULTS_SIMULATOR
-  // Settings only for Grbl Simulator (www.github.com/grbl/grbl-sim)
-  #include "defaults/defaults_simulator.h"
-#endif
+// Generic conservative settings for a RAMP CNC machine. You must update these yourself.
+// Keep in mind that Grbl is highly efficient and settings can be significantly different.
+// Especially when optimizing for a different CNC task like going from 3d printing to
+// CNC milling or laser cutting. Unlike Marlin, these defaults are only applied when the
+// EEPROM is explicitly wiped, either by a `$RST=*` command or Grbl detecting a settings
+// version type change (not frequent).
+#define DEFAULT_X_STEPS_PER_MM 80.0
+#define DEFAULT_Y_STEPS_PER_MM 80.0
+#define DEFAULT_Z_STEPS_PER_MM 80.0
+#define DEFAULT_X_MAX_RATE 2500.0 // mm/min
+#define DEFAULT_Y_MAX_RATE 2500.0 // mm/min
+#define DEFAULT_Z_MAX_RATE 500.0 // mm/min
+#define DEFAULT_X_ACCELERATION (100.0*60.0*60.0) // 10*60*60 mm/min^2 = 10 mm/sec^2
+#define DEFAULT_Y_ACCELERATION (100.0*60.0*60.0) // 10*60*60 mm/min^2 = 10 mm/sec^2
+#define DEFAULT_Z_ACCELERATION (100.0*60.0*60.0) // 10*60*60 mm/min^2 = 10 mm/sec^2
+#define DEFAULT_X_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_Y_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_Z_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
+#define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
+#define DEFAULT_STEP_PULSE_MICROSECONDS 10
+#define DEFAULT_STEPPING_INVERT_MASK 0
+#define DEFAULT_DIRECTION_INVERT_MASK 0
+#define DEFAULT_STEPPER_IDLE_LOCK_TIME 255 // msec (0-254, 255 keeps steppers enabled)
+#define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
+#define DEFAULT_JUNCTION_DEVIATION 0.02 // mm
+#define DEFAULT_ARC_TOLERANCE 0.002 // mm
+#define DEFAULT_REPORT_INCHES 0 // false
+#define DEFAULT_INVERT_ST_ENABLE 0 // false
+#define DEFAULT_INVERT_LIMIT_PINS 0 // false
+#define DEFAULT_SOFT_LIMIT_ENABLE 0 // true
+#define DEFAULT_HARD_LIMIT_ENABLE 0  // false
+#define DEFAULT_INVERT_PROBE_PIN 0 // false
+#define DEFAULT_LASER_MODE 0 // false
+#define DEFAULT_HOMING_ENABLE 0  // true
+#define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
+#define DEFAULT_HOMING_FEED_RATE 500.0 // mm/min
+#define DEFAULT_HOMING_SEEK_RATE 2000.0 // mm/min
+#define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
+#define DEFAULT_HOMING_PULLOFF 1.0 // mm
 
 #endif
